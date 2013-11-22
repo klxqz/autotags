a0b7af6dfe97230240dc97a1fee8624c7c9eaa5d
klxqz <klxqz@mail.ru>
2013-11-22 14:10:17 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,18 +3,17 @@
 return array(
     'name' => 'Автотеги',
     'description' => 'Автоматическое заполнение мета-тегов',
-    'vendor'=>903438,
-    'version'=>'1.0.2',
-    'img'=>'img/autotags.png',
+    'vendor' => '985310',
+    'version' => '1.0.3',
+    'img' => 'img/autotags.png',
     'shop_settings' => true,
-    'frontend'    => true,
-    'icons'=>array(
-        16=>'img/autotags.png',
+    'frontend' => true,
+    'icons' => array(
+        16 => 'img/autotags.png',
     ),
     'handlers' => array(
         'frontend_product' => 'frontendProduct',
         'frontend_category' => 'frontendCategory',
     ),
-
 );
 //EOF
